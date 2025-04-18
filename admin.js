8af4bb0bb8fbd5eed428254f765b3db59500c642
pablorozados <pablorozados@proton.me>
2025-04-18 16:42:18 -0300

    Corrigindo tudo

--- admin.js
+++ admin.js
@@ -1,11 +1,7 @@
 // Configuração do Firebase (será substituída no deploy)
 const firebaseConfig = {
   apiKey: "FIREBASE_API_KEY",
-  authDomain: "reliquias-do-xis.firebaseapp.com",
   projectId: "FIREBASE_PROJECT_ID",
-  storageBucket: "reliquias-do-xis.appspot.com",
-  messagingSenderId: "936551505510",
-  appId: "1:936551505510:web:22de1482a8f8d9720257a7"
 };
 
 // Inicialização do Firebase
