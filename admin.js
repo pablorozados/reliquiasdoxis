4decfd31a8be8384d442c4e3cc5cc87a7bf11a5b
pablorozados <pablorozados@proton.me>
2025-04-24 13:22:42 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,11 +1,8 @@
-// Configuração completa do Firebase
+// Configuração do Firebase (versão simplificada para seus 3 secrets)
 const firebaseConfig = {
   apiKey: "FIREBASE_API_KEY",
   authDomain: "FIREBASE_PROJECT_ID.firebaseapp.com",
-  projectId: "FIREBASE_PROJECT_ID",
-  storageBucket: "FIREBASE_PROJECT_ID.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  projectId: "FIREBASE_PROJECT_ID"
 };
 
 // Inicialização do Firebase
