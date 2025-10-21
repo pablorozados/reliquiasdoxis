fc4bb01a9f11d49e1da41460414c141909bbcaad
pabloprime <pablorozados@gmail.com>
2025-10-21 15:36:48 -0300

    Adicionado config.prod.js para ambiente de produção

---
+++ config.prod.js
@@ -0,0 +1,3 @@
+// config.prod.js - Configuração para produção
+const FIREBASE_API_KEY = "AIzaSyAEcQDgKssjXZQ54Bsid3vPIKiGm36l4wM";
+const GOOGLE_MAPS_API_KEY = "AIzaSyBjfkEwQ_gpYGjRlY9deWCoddEmrBNOQDc";
