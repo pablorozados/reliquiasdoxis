8533565726724c1413068d80b6d4d677af928528
pabloprime <pablorozados@gmail.com>
2026-01-16 10:51:39 -0300

    fix: atualizar Firebase config com as chaves corretas do Console

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 // config.js - Configuração do Firebase para o browser
 const config = {
     firebase: {
-        apiKey: "AIzaSyAEcQDgKssjXZQ54Bsid3vPIKiGm36l4wM",
-        authDomain: "reliquiasdoxis.firebaseapp.com",
-        projectId: "reliquiasdoxis",
-        storageBucket: "reliquiasdoxis.appspot.com",
-        messagingSenderId: "673027539850",
-        appId: "1:673027539850:web:c8c5fa9e5dbff158cf92ed"
+        apiKey: "AIzaSyA7_SFUPE6n9KG6LhL9Y6DRanSW5Zn0-2k",
+        authDomain: "reliquias-do-xis.firebaseapp.com",
+        projectId: "reliquias-do-xis",
+        storageBucket: "reliquias-do-xis.firebasestorage.app",
+        messagingSenderId: "936551505510",
+        appId: "1:936551505510:web:22de1482a8f8d9720257a7",
+        measurementId: "G-RBVHF6R01W"
     },
     googleMaps: {
         apiKey: "AIzaSyBjfkEwQ_gpYGjRlY9deWCoddEmrBNOQDc"
